a26dda521bbbc314c0f316c3723586e0ef29358d
okonkwo348 <okonkwopaul348@gmail.com>
2026-03-27 16:55:52 +0100

    update on base conversion

---
+++ hackathon_day2/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Hex -> Dec
+func hex(s string) string {
+	a, err := strconv.ParseInt(s, 16, 64)
+	if err != nil {
+		return "invalid input"
+	}
+	return strconv.FormatInt(a, 10)
+}
+
+// Bin -> Dec
+func bin(s string) string {
+	a, err := strconv.ParseInt(s, 2, 64)
+	if err != nil {
+		return "invalid input"
+	}
+	return strconv.FormatInt(a, 10)
+
+}
+
+// Dec -> Hex
+func decHex(s int64) string {
+
+	return strconv.FormatInt(s, 16)
+}
+
+func decBin(s int64) string {
+
+	return strconv.FormatInt(s, 2)
+}
+
+func main() {
+start:
+	scanner := bufio.NewScanner(os.Stdin)
+
+	if !scanner.Scan() {
+		goto start
+	}
+
+	line := scanner.Text()
+	part := strings.Fields(line)
+
+	if len(part) != 3 {
+		fmt.Println("")
+		goto start
+	}
+
+	if part[0] == "quit" {
+		fmt.Println("exit")
+	}
+
+	if part[0] == "HELP" || part[0] == "help" {
+		fmt.Println("Hex -> Dec do : convert 1E hex")
+		fmt.Println()
+		goto start
+	}
+
+	if len(part) == 3 && part[0] == "convert" && strings.ContainsAny(part[1], "0123456789ABCDEF") || part[2] == "hex" || part[2] == "bin" || part[2] == "dec" {
+		switch part[2] {
+		case "bin":
+			fmt.Println(bin(part[1]))
+		case "hex":
+			fmt.Println(hex(part[1]))
+		case "dex":
+			fmt.Println(part[1])
+		default:
+			fmt.Println("out the given functions")
+		}
+	}
+}
