43cfdd326a95e13dcf513cc89e918a83b84bd33c
okonkwo348 <okonkwopaul348@gmail.com>
2026-04-23 11:21:26 +0100

    next commit

--- printstr.go
+++ printstr.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // func PrintStr(s string) {
 
@@ -11,11 +13,71 @@ import "fmt"
 // 	PrintStr("Hello World!")
 // }
 
-func StrLen(s string) int {
-	return len(s)
+// func StrLen(s string) int {
+// 	return len(s)
+// }
+
+// func main() {
+// 	fmt.Println(StrLen("Hello World!"))
+
+// }
+
+// func Swap(a *int, b *int) {
+// 	s := *a
+// 	t := *b
+// 	*b = s
+// 	*a = t
+
+// }
+
+// func main() {
+// 	a := 0
+// 	b := 1
+// 	Swap(&a, &b)
+// 	fmt.Println(a)
+// 	fmt.Println(b)
+// }
+
+// func StrRev(s string) string {
+// 	a := []rune(s)
+// 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+// 		a[i], a[j] = a[j], a[i]
+// 	}
+// 	return string(a)
+// }
+
+// func StrRev(s string) string {
+// 	a := []rune(s)
+// 	c := []rune{}
+// 	for i := len(a) - 1; i >= 0; i-- {
+// 		c = append(c, a[i])
+// 	}
+// 	return string(c)
+// }
+
+// func main() {
+// 	s := "Hello world"
+// 	result := StrRev(s)
+// 	fmt.Println(result)
+// }
+
+// func BasicAtoi(s string) int {
+// 	file, _ := strconv.Atoi(s)
+// 	return file
+// }
+
+func BasicAtoi(s string) int {
+	result := 0
+
+	for _, char := range s {
+		digit := int(char - '0')
+		result = result*10 + digit
+	}
+	return result
 }
 
 func main() {
-	fmt.Println(StrLen("Hello World!"))
-
+	fmt.Println(BasicAtoi("12345"))
+	fmt.Println(BasicAtoi("0000000012345"))
+	fmt.Println(BasicAtoi("000000"))
 }
