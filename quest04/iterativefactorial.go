9a23477fad7e17e76523a3c6307f4e6989cb49d8
okonkwo348 <okonkwopaul348@gmail.com>
2026-04-23 23:01:12 +0100

    next commit

--- quest04/iterativefactorial.go
+++ quest04/iterativefactorial.go
@@ -36,3 +36,40 @@ Errors (non possible values or overflows) will return 0.*/
 // 	arg := 4
 // 	fmt.Println(RecursiveFactorial(arg))
 // }
+
+/* Write an iterative function that returns the value of nb to the power of power.
+
+Negative powers will return 0. Overflows do not have to be dealt with.*/
+
+// func IterativePower(nb int, power int) int {
+// 	num := 1
+// 	for i := 0; i < power; i++ {
+// 		num *= nb
+// 	}
+// 	return num
+// }
+
+// func main() {
+// 	fmt.Println(IterativePower(3, 4))
+// }
+
+/* Write a recursive function that returns the value of nb to the power of power.
+
+Negative powers will return 0. Overflows do not have to be dealt with.
+
+for is forbidden for this exercise.*/
+
+// func RecursivePower(nb int, power int) int {
+// 	num := 1
+
+// 	i := 0
+// 	for i < power {
+// 		i++
+// 		num *= nb
+// 	}
+// 	return num
+// }
+
+// func main() {
+// 	fmt.Println(RecursivePower(4, 3))
+// }
