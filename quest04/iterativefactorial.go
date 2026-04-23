a24870364e301b140c6e229de1c057c61b5d44bf
okonkwo348 <okonkwopaul348@gmail.com>
2026-04-23 22:52:49 +0100

    next commit

---
+++ quest04/iterativefactorial.go
@@ -0,0 +1,38 @@
+package main
+
+/* Write an iterative function that returns the factorial of the int passed as parameter.
+
+Errors (non possible values or overflows) will return 0.*/
+// func IterativeFactorial(nb int) int {
+// 	num := 1
+// 	for i := 1; i <= nb; i++ {
+// 		num *= i
+// 	}
+// 	return num
+// }
+
+// func main() {
+// 	arg := 4
+// 	fmt.Println(IterativeFactorial(arg))
+// }
+
+/* Write a recursive function that returns the factorial of the int passed as parameter.
+
+Errors (non possible values or overflows) will return 0.*/
+
+// func RecursiveFactorial(nb int) int {
+
+// 	num := 1
+// 	i := 0
+
+// 	for i < nb {
+// 		i++
+// 		num *= i
+// 	}
+// 	return num
+// }
+
+// func main() {
+// 	arg := 4
+// 	fmt.Println(RecursiveFactorial(arg))
+// }
