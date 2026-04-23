c75c84974f73abd2f1239c4c0db07617a5fa63be
okonkwo348 <okonkwopaul348@gmail.com>
2026-04-23 23:39:11 +0100

    next commit

--- quest04/iterativefactorial.go
+++ quest04/iterativefactorial.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /* Write an iterative function that returns the factorial of the int passed as parameter.
 
 Errors (non possible values or overflows) will return 0.*/
@@ -73,3 +75,44 @@ for is forbidden for this exercise.*/
 // func main() {
 // 	fmt.Println(RecursivePower(4, 3))
 // }
+
+/* Write a recursive function that returns the value at the position index in the fibonacci sequence.
+
+The first value is at index 0.
+
+The sequence starts this way: 0, 1, 1, 2, 3 etc...
+
+A negative index will return -1.
+
+for is forbidden for this exercise.*/
+
+// func Fibonacci(index int) int {
+// 	num := []int{4, 1, 7, 2, 3, 8}
+// 	x := 0
+// 	for i := 0; i < len(num); i++ {
+// 		if i == index {
+// 			x += num[i]
+// 			break
+// 		}
+// 	}
+// 	return x
+// }
+
+// func main() {
+// 	arg1 := 5
+// 	fmt.Println(Fibonacci(arg1))
+// }
+
+func IsPrime(nb int) bool {
+
+	if nb%2 == 0 || nb%3 == 0 || nb%5 == 0 || nb%7 == 0 || nb%11 == 0 {
+		return false
+	}
+	return true
+
+}
+
+func main() {
+	fmt.Println(IsPrime(110))
+	fmt.Println(IsPrime(9))
+}
