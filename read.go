3c5eafeaf3b7cacb246103f5cfc3098e5b42e3fb
okonkwo348 <okonkwopaul348@gmail.com>
2026-06-08 13:59:13 +0100

    read File

--- read.go
+++ read.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"os"
 )
 
-func ReadFile(name string) ([]byte, error){
-	input := os.Args
+func ReadFile(name string) (string, error) {
+
 	file, err := os.ReadFile(name)
-	if err = nil{
-		return nil, errors.New("file Not Found")
+	if err != nil {
+		return "", errors.New("file Not Found")
 	}
 
-	return file, nil
+	return string(file), nil
+}
+
+func main() {
+	input := os.Args
+
+	if input[1] == "" {
+		println("No argument is provided")
+	}
+
+	data, err := ReadFile(input[1])
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+
+	fmt.Println(data)
+
 }
