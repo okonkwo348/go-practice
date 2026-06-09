30b11bb65ca9c01930d1c4ee32b054b980ece78a
okonkwo348 <okonkwopaul348@gmail.com>
2026-06-09 10:08:21 +0100

    quote func

--- quote.go
+++ quote.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"fmt"
-	"regexp"
+	"strings"
+	"unicode"
 )
 
 func Quote(s string) string {
-	last := regexp.MustCompile("'*s/([^'])*s/'")
-	s = last.ReplaceAllString(s, "'$1'")
+	p := strings.Fields(s)
+	k := strings.Join(p, " ")
+	var b strings.Builder
+	for i := 0; i < len(k); i++ {
+		if string(k[i]) == " " && unicode.IsPunct(rune(k[i-1])) {
+			b.WriteString(string(k[i+1]))
+			i++
+			continue
+		}
 
-	return s
+		if string(k[i]) == " " && unicode.IsPunct(rune(k[i+1])) {
+			b.WriteString(string(k[i+1]))
+			i++
+			continue
+		}
+
+		b.WriteString(string(k[i]))
+	}
+
+	return b.String()
 }
 
 func main() {
-	fmt.Println(Quote(" ' awasome '"))
+	fmt.Println(Quote(" '                   awasome      '"))
+	fmt.Println(Quote(" '   hello world     '"))
 }
