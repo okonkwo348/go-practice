8f6bd1c1c5122afc18f9f2aeca2ab5cca8ab0487
okonkwo348 <okonkwopaul348@gmail.com>
2026-06-16 21:41:14 +0100

    second commit

--- main.go
+++ main.go
@@ -22,36 +22,49 @@ import (
 // 	}
 // }
 
-func okHandler(w http.ResponseWriter, r *http.Request){
-	 
-		http.Error(w, "Successful", 200)
+// func okHandler(w http.ResponseWriter, r *http.Request){
+// 		http.Error(w, "Successful", 200)
 		
-}
+// }
 
-func nfHandler(w http.ResponseWriter, r *http.Request){
-	 
-		http.Error(w, "Not Found", 404)
+// func nfHandler(w http.ResponseWriter, r *http.Request){
+// 		http.Error(w, "Not Found", 404)
 		
-}
+// }
 
-func brHandler(w http.ResponseWriter, r *http.Request){
-	 
-		http.Error(w, "Bad Request", 400)
+// func brHandler(w http.ResponseWriter, r *http.Request){
+// 		http.Error(w, "Bad Request", 400)
 		
-}
+// }
 
-func erHandler(w http.ResponseWriter, r *http.Request){
-	
-	http.Error(w, "500 internal server error", 500)
+// func erHandler(w http.ResponseWriter, r *http.Request){
+// 	http.Error(w, "500 internal server error", 500)
 	
 	
-}
+// }
+
+// func main(){
+// 	http.HandleFunc("/ok", okHandler)
+// 	http.HandleFunc("/notfound", nfHandler)
+// 	http.HandleFunc("/badrequest", brHandler)
+// 	http.HandleFunc("/error", erHandler)
+// 	fmt.Println("Server running on http//:localhost:8080")
+// 	http.ListenAndServe(":8080",nil)
+// }
 
 func main(){
-	http.HandleFunc("/ok", okHandler)
-	http.HandleFunc("/notfound", nfHandler)
-	http.HandleFunc("/badrequest", brHandler)
-	http.HandleFunc("/error", erHandler)
+	http.HandleFunc("/ok", func(w http.ResponseWriter, r *http.Request){
+		http.Error(w, "Successful", 200)
+	})
+	http.HandleFunc("/notfound", func(w http.ResponseWriter, r *http.Request){
+		http.Error(w, "Not Found", 404)
+	})
+	http.HandleFunc("/badrequest", func(w http.ResponseWriter, r *http.Request){
+		http.Error(w, "Bad Request", 400)
+	})
+	http.HandleFunc("/error", func(w http.ResponseWriter, r *http.Request){
+		http.Error(w, "500 internal server error", 500)
+	})
 	fmt.Println("Server running on http//:localhost:8080")
 	http.ListenAndServe(":8080",nil)
 }
