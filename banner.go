f550fb1c4f74bc4b62d6752c144c8dbb17d12ddc
okonkwo348 <okonkwopaul348@gmail.com>
2026-06-18 01:54:23 +0000

    added edge cases

--- banner.go
+++ banner.go
@@ -9,11 +9,19 @@ import (
 func LoadBanner(filename string) (map[rune][]string, error) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
-		return nil, errors.New("error")
+		return nil, errors.New("error in reading file")
 	}
 
 	bannerLines := strings.Split(string(data), "\n")
+    
+    if len(bannerLines)==0{
+        return nil, errors.New("Empty File")
+    }
 
+    if len(bannerLines)<855{
+        return nil, errors.New("incomplete lines")
+    }
+    
 	var lines []string
 	mapLine := make(map[rune][]string)
 	for i := 0; i < 95; i++ {
